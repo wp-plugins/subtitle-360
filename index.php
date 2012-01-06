ac3247a72712f92bf3580150b460e2c8af3f1427
HasanulBanna <HasanulBanna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-06 11:22:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/subtitle-360/trunk@485652 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package Subtitle 360
+ * @version 1.0
+ */
+/*
+Plugin Name: Subtitle 360
+Plugin URI: #
+Description: This plugin is used to create subtitle for pages and post. Now ou can use the subtitle on your page. If you have a big tagline for each page here is the solution.
+Author: Hasanul Banna
+Version: 1.0
+Author URI: http://hasanul.banna360.com/
+*/
+?>
+<?php    
+    add_action( 'admin_menu', 'my_create_post_meta_box' );
+    add_action( 'save_post', 'my_save_post_meta_box', 10, 2 );
+
+function my_create_post_meta_box() {
+    add_meta_box( 'my-meta-box', 'Post Sub Title', 'my_post_meta_box', 'post', 'normal', 'high' );
+    add_meta_box( 'my-meta-box', 'Page Sub Title', 'my_post_meta_box', 'page', 'normal', 'high' );
+}
+
+function my_post_meta_box( $object, $box ) { ?>
+    <div id="postcustomstuff">
+    <p>
+    
+        <label>Please enter sub title:</label>
+        <input name="page_sub_title" id="sw_title" style="width: 97%;" value="<?php echo wp_specialchars( get_post_meta( $object->ID, 'page_sub_title', true ), 1 ); ?>" />
+        <input type="hidden" name="my_meta_box_nonce" value="<?php echo wp_create_nonce( plugin_basename( __FILE__ ) ); ?>" />
+    </p>
+    </div>
+<?php }
+
+function my_save_post_meta_box( $post_id, $post ) {
+
+    if ( !wp_verify_nonce( $_POST['my_meta_box_nonce'], plugin_basename( __FILE__ ) ) )
+        return $post_id;
+
+    if ( !current_user_can( 'edit_post', $post_id ) )
+        return $post_id;
+
+    //Saving 1st Data
+    
+    $meta_value = get_post_meta( $post_id, 'page_sub_title', true );
+    $new_meta_value = stripslashes( $_POST['page_sub_title'] );
+
+    if ( $new_meta_value && '' == $meta_value )
+        add_post_meta( $post_id, 'page_sub_title', $new_meta_value, true );
+
+    elseif ( $new_meta_value != $meta_value )
+        update_post_meta( $post_id, 'page_sub_title', $new_meta_value );
+
+    elseif ( '' == $new_meta_value && $meta_value )
+        delete_post_meta( $post_id, 'page_sub_title', $meta_value ); 
+}
+function the_subtitle() {
+		global $post;
+		echo '<h4 class="subtitle_head">';
+		echo get_post_meta($post->ID, 'page_sub_title', true);
+		echo '</h4>';
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+﻿=== Plugin Name ===
+Contributors: HasanulBanna
+Tags: page title, subtitle, page sub title
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+This plugin creates a option to enter sub heading for pages and posts. You can display the sub title in your theme by using the <code><?php if (function_exists('the_subtitle')){ the_subtitle(); }?></code> template tag.
+
+== Description ==
+
+This plugin creates a option to enter sub heading for pages and posts. You can display the sub title in your theme by using the <code><?php if (function_exists('the_subtitle')){ the_subtitle(); }?></code> template tag.
+
+== Installation ==
+
+1. Upload the entire `subtitle-360` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Now under pages and posts you can see option to enter sub title.
+
+== Frequently Asked Questions ==
+
+= My Sub title won't upload. What should I do? =
+
+You may be forgot to add template code to the area you want to display subtitle.
+<code><?php if (function_exists('the_subtitle')){ the_subtitle(); }?></code>
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
